ff799076c00399e94b4d1fff735749d23d5ad402
Anmol Saini <anmolsaini@Anmols-MacBook-Pro.local>
2022-01-19 10:36:18 -0500

    Commented Dice Rolls Simulation

--- CS 3100/Simulations/Dice Rolls/project1.cpp
+++ CS 3100/Simulations/Dice Rolls/project1.cpp
@@ -2,34 +2,42 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define ROLL_COMBINATIONS 11
+#define ROLL_COMBINATIONS 11 // 11 possible outcomes when 2 dice are rolled
 using namespace std;
 
 int main() {
     
-    int numRolls;
+    int numRolls; // number of times to roll 2 dice as determined by the user
     cout << "How many rolls? ";
     cin >> numRolls;
     cout << "Simulating " << numRolls << " rolls..." << endl;
     cout << "Results:" << endl;
-
+ 
+    // ---------------------------------------------------------------------------
+    // creates an int array of length 11 to hold all the results of the dice rolls
+    // initializes each value in the array to 0
     int rolls[ROLL_COMBINATIONS];
     for (int i = 0; i < ROLL_COMBINATIONS; i++) {
         rolls[i] = 0;
-        //cout << rolls[i] << endl;
     }
+    // ---------------------------------------------------------------------------
 
-    srand(time(NULL));
+    srand(time(NULL)); // seeds the random number generator with the time
 
+    // ----------------------------------------------------
+    // performs dice rolls and records results in the array
     for (int i = 0; i < numRolls; i++) {
         int firstToss = (rand() % 6) + 1;
         int secondToss = (rand() % 6) + 1;
         int rollResult = firstToss + secondToss;
-        //cout << "rollResult: " << rollResult << endl;
         rolls[rollResult-2]++;
     }
+    // ----------------------------------------------------
 
+    // ---------------------------------------------------------------
+    // displays all results
     for (int i = 0; i < ROLL_COMBINATIONS; i++) {
         cout << i+2 << " was rolled " << rolls[i] << " times" << endl;
     }
+    // ---------------------------------------------------------------
 }
